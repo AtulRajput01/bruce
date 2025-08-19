ba2ab5f443fccf482479b13bf0605ddc027335da
atul.rajput <atul.rajput@sparxitsolutions.com>
2025-08-19 17:09:33 +0530

    fix chatbot

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import './App.css';
 
-const API_URL = 'http://localhost:3001';
+const API_URL = 'https://atulrajput.site/api';
 
 function App() {
   const [systemResources, setSystemResources] = useState(null);
